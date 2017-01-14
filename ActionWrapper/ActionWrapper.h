e78a6b2b14d6e49ac8afe9d445260dbfbd9109fb
Anton Pomozov <pomozoff@mail.ru>
2017-01-14 21:41:40 +0300

    Initial Commit

---
+++ ActionWrapper/ActionWrapper.h
@@ -0,0 +1,19 @@
+//
+//  ActionWrapper.h
+//  ActionWrapper
+//
+//  Created by Anton Pomozov on 14/01/2017.
+//  Copyright © 2017 Anton Pomozov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActionWrapper.
+FOUNDATION_EXPORT double ActionWrapperVersionNumber;
+
+//! Project version string for ActionWrapper.
+FOUNDATION_EXPORT const unsigned char ActionWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionWrapper/PublicHeader.h>
+
+
